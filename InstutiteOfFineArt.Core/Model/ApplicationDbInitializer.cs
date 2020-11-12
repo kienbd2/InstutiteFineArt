f115236214d70493d91d8364e03ff982ddfafc25
BuiKien <kienbd@hopnhat.vn>
2020-11-12 18:00:18 +0700

    Add project files.

---
+++ InstutiteOfFineArt.Core/Model/ApplicationDbInitializer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace InstutiteOfFineArt.Core.Model
+{
+    public class ApplicationDbInitializer : CreateDatabaseIfNotExists<InstutiteFineArtDbContext>
+    {
+        protected override void Seed(InstutiteFineArtDbContext context)
+        {
+            InitializeIdentityForEF(context);
+            context.SaveChanges();
+            base.Seed(context);
+            
+            base.Seed(context);
+        }
+
+        //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
+        public static void InitializeIdentityForEF(InstutiteFineArtDbContext db)
+        {
+            var userManager = new UserManager<User>(new UserStore<User>(db));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            const string name = "admin@example.com";
+            const string password = "Admin@123456";
+            const string roleName = "Admin";
+
+            //Create Role Admin if it does not exist
+            var role = roleManager.FindByName(roleName);
+            if (role == null)
+            {
+                role = new IdentityRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new User { UserName = name, Email = name };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+
+            // Add user admin to Role Admin if not already added
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name))
+            {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+}
---
+++ InstutiteOfFineArt.Core/Model/InstutiteFineArtDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace InstutiteOfFineArt.Core.Model
+{
+    class InstutiteFineArtDbContext : IdentityDbContext<User>
+    {
+        public InstutiteFineArtDbContext()
+           : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        static InstutiteFineArtDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<InstutiteFineArtDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static InstutiteFineArtDbContext Create()
+        {
+            return new InstutiteFineArtDbContext();
+        }
+    }
+}
