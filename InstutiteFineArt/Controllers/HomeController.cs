54ed6737b6055b9bb7176a0d8d291638bd5f9532
ADMIN <ADMIN@DESKTOP-0GHR3KF>
2020-11-28 00:21:19 +0700

    update view home

--- InstutiteFineArt/Controllers/HomeController.cs
+++ InstutiteFineArt/Controllers/HomeController.cs
@@ -1,13 +1,68 @@
-﻿using System.Web.Mvc;
+﻿using IdentitySample.Models;
+using InstutiteOfFineArt.DAL.Repository;
+using Microsoft.AspNet.Identity.Owin;
+using PagedList;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace IdentitySample.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
-        public ActionResult Index()
+        private readonly UserRepository _repoUser;
+        public HomeController()
         {
-            return View();
+            _repoUser = new UserRepository();
+        }
+        public HomeController(ApplicationUserManager userManager, ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private ApplicationRoleManager _roleManager;
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+        [HttpGet]
+        public ActionResult Index(int? size, int? page, string searchString)
+        {
+            
+            ViewBag.CurrentFilter = searchString;
+
+            var role = RoleManager.Roles.FirstOrDefault(x => x.Name == "Staff");
+            var lstUserId = _repoUser.FindAll(x => x.RoleId == role.Id).Select(x => x.UserId).ToList();
+            var lstUser = UserManager.Users.Where(x => lstUserId.Any(p => p.Contains(x.Id))).ToList();
+            ViewBag.stt = 1;
+            ViewBag.currentSize = size; // tạo biến kích thước trang hiện tại
+            ViewBag.total = lstUser.Count;
+            page = page ?? 1;
+            int pageSize = (size ?? 4);
+            int pageNumber = (page ?? 1);
+            return View(lstUser.ToPagedList(pageNumber, pageSize));
         }
 
         [HttpGet]
